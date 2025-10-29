b02979e162a175d4e05b6f0acfd1106ea050aecc
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-10-29 16:03:33 +0100

    Add Image model and DbSet to API

---
+++ Api/Models/Image.cs
@@ -0,0 +1,10 @@
+﻿namespace Models;
+
+public class Image
+{
+    public int Id { get; set; }
+    public string FileName { get; set; } = string.Empty;
+
+    public int? ToDontId { get; set; }
+    public ToDont? ToDont { get; set; }
+}
