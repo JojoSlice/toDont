9cd10bdf1ee2ada5c452d5bdb809e440d0ca0337
JojoSlice <Johannes152@hotmail.com>
2025-10-30 13:40:01 +0100

    correct namespace, added missing using

--- Api/Models/User.cs
+++ Api/Models/User.cs
@@ -1,7 +1,8 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Models;
 
-namespace Models
+namespace Api.Models
 {
     public class User
     {
