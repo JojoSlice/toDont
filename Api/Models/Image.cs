3412809c78f5ace2cfec14f18282a82dbb2ebd7e
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-10-30 13:31:28 +0100

    fixat lite men [DatabaseGenerated(DatabaseGeneratedOption.Identity)] gick inte hem riktigt

--- Api/Models/Image.cs
+++ Api/Models/Image.cs
@@ -1,10 +1,14 @@
-﻿namespace Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models;
 
 public class Image
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
     public string FileName { get; set; } = string.Empty;
-
     public int? ToDontId { get; set; }
     public ToDont? ToDont { get; set; }
 }
