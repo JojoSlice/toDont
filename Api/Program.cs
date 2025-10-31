bd1e772cdc71d8058b72914998071e22c90c2190
JojoSlice <Johannes152@hotmail.com>
2025-10-31 14:38:32 +0100

    null check on jwt key

--- Api/Program.cs
+++ Api/Program.cs
@@ -29,7 +29,8 @@ builder
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
             ValidAudience = builder.Configuration["Jwt:Audience"],
             IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]
+                    ?? throw new InvalidOperationException("JWT Key not configured"))
             ),
         };
     });
